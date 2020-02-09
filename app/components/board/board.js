94093c95d63a0ede615a504b6f92680ad1cd5cf8
Sebastian van Hesteren <s.vanhesteren@gmail.com>
2020-02-09 17:34:22 +0100

    Add onClick function as argument to Tile
    The logic remains at Board where the state get updated. The function is
    passed as an onClick argument to Tile.

--- app/components/board/board.js
+++ app/components/board/board.js
@@ -1,12 +1,20 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class BoardBoardComponent extends Component {
   @tracked state = {
-    tiles: [
-      null, null, null,
-      null, null, null,
-      null, null, null
-    ]
+    tiles: Array(9).fill(null)
+  }
+
+  @action placeMove(pos) {
+    let tiles = this.state.tiles;
+    tiles[pos] = "X";
+    this.state = {
+      ...this.state,
+      tiles: this.state.tiles.map((tile, index) => {
+        return index === pos ? "X" : tile
+      }
+    )}
   }
 }
