0232ee4390e1da413953cfaef3f6af8de3a9591a
Sebastian van Hesteren <s.vanhesteren@gmail.com>
2020-02-09 15:16:57 +0100

    add basic tile component

---
+++ app/components/tile/tile.js
@@ -0,0 +1,5 @@
+import Component from '@glimmer/component';
+
+export default class TileTileComponent extends Component {
+  icon = "";
+}
---
+++ tests/integration/components/tile/tile-test.js
@@ -0,0 +1,16 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | tile/tile', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders a tile', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`<Tile::Tile />`);
+    assert.dom('.tile').exists();
+  });
+});
