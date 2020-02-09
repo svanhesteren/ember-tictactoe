112d955dc0708595e0a394d4b1cd4d333ea31795
Sebastian van Hesteren <s.vanhesteren@gmail.com>
2020-02-09 22:33:25 +0100

    add a test for draw situation

--- tests/integration/components/board/board-test.js
+++ tests/integration/components/board/board-test.js
@@ -38,4 +38,26 @@ module('Integration | Component | board/board', function(hooks) {
     assert.dom(tiles[1]).hasNoText();
     assert.dom(this.element).containsText("Player turn: O");
   })
+
+  test('all tiles filled with no winning line is a draw, resets the game when clicking again', async function(assert) {
+    await render(hbs`<Board::Board />`);
+    const tiles = findAll('.tile');
+    await click(tiles[0]); // x
+    await click(tiles[1]); // o
+    await click(tiles[2]); // x
+
+    await click(tiles[3]); // o
+    await click(tiles[5]); // x
+    await click(tiles[4]); // o
+
+    await click(tiles[6]); // x
+    await click(tiles[8]); // o
+    await click(tiles[7]); // x
+    assert.dom(this.element).containsText("Its a draw!")
+    assert.dom(this.element).containsText("Draw: 1")
+    await click(tiles[0]);
+    assert.dom(tiles[0]).hasText("X");
+    assert.dom(tiles[1]).hasNoText();
+    assert.dom(this.element).containsText("Player turn: O");
+  })
 });
